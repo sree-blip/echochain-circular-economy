12ff5d878f81b261f7bbbe3c88a310b6c9c89ae9
Talmi-web_scraping_engineer <k.sritalmi.06@gmail.com>
2026-07-16 14:51:56 +0530

    Day 6 :Added product scraping logic and generates products data

--- ecommerce_scraper/spiders/products.py
+++ ecommerce_scraper/spiders/products.py
@@ -1,14 +1,20 @@
 import scrapy
 
-class ProductsSpider(scrapy.Spider):
+class ProductSpider(scrapy.Spider):
     name = "products"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/"]
+    start_urls = [
+        "https://example.com/products"
+    ]
 
     def parse(self, response):
-        titles = response.css("h3 a::attr(title)").getall()
+        products = response.css("div.product")
 
-        for title in titles:
-            yield {
-                "title": title
-            }
+        for product in products:
+            name = product.css("h2::text").get()
+            price = product.css(".price::text").get()
+
+            if name and price:
+                yield {
+                    "product_name": name.strip(),
+                    "price": price.strip()
+                }
