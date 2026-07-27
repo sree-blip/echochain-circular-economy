581ab54fe24e01816ed75411110ccae596afcbba
sri talmi kandru <k.sritalmi.06@gmail.com>
2026-07-27 23:56:02 +0530

    Day 7 Pagination handled and scraper data dataset exported successfully

--- echochain/echochain/spiders/products.py
+++ echochain/echochain/spiders/products.py
@@ -1,25 +1,22 @@
 import scrapy
-import random
 
 class ProductsSpider(scrapy.Spider):
     name = "products"
-    start_urls = ["https://example.com"]
+    
+    # Pagination loop (1 to 5 pages)
+    def start_requests(self):
+        for page in range(1, 5):
+            url = f"https://echochain-e-commerce-ca8d.bolt.host/?page={page}"
+            yield scrapy.Request(url=url, callback=self.parse, meta={'page_number': page})
 
     def parse(self, response):
-
-        categories = [
-            "Mobile",
-            "Laptop",
-            "Furniture",
-            "Electronics",
-            "Accessories"
-        ]
-
-        for i in range(1, 61):
-
+        page_num = response.meta.get('page_number', 1)
+        products = response.css("div.product, div.card, article")
+        
+        for prod in products:
             yield {
-                "product_id": i,
-                "product_name": f"Product_{i}",
-                "category": random.choice(categories),
-                "price": random.randint(1000, 50000)
+                "product_name": prod.css("h1::text, h2::text, h3::text, .title::text").get(),
+                "resale_price": prod.css("span.price::text, .price::text").get(),
+                "page_number": page_num,
+                "source_url": response.url
             }
