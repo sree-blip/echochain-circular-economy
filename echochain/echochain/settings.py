0a45e17b895b30918f7463ef0159ce7f169e5d2c
sri talmi kandru <k.sritalmi.06@gmail.com>
2026-07-28 14:28:40 +0530

    Revert "Merge pull request #45 from sree-blip/Talmi-web_scraping_engineer"
    This reverts commit 59004b92ed4de1e4cee9105522336932ab75dcac, reversing
    changes made to 762e31c55988b68312bbf194739043617388b988.

--- echochain/echochain/settings.py
+++ echochain/echochain/settings.py
@@ -19,7 +19,7 @@ ADDONS = {}
 #USER_AGENT = "echochain (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Concurrency and throttling settings
 #CONCURRENT_REQUESTS = 16
